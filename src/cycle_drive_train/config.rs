6ab5d39082b5f02d841458e38d491f23e8306e1c
zehreken <zehreken@gmail.com>
2021-07-10 23:31:50 +0200

    Improve wheel and gear math

--- src/cycle_drive_train/config.rs
+++ src/cycle_drive_train/config.rs
@@ -1,2 +1,4 @@
 pub const WINDOW_WIDTH: i32 = 700;
 pub const WINDOW_HEIGHT: i32 = 512;
+pub const FRONT_GEAR_SIZE: f32 = 46.0;
+pub const REAR_GEAR_SIZE: f32 = 16.0;
--- src/cycle_drive_train/mod.rs
+++ src/cycle_drive_train/mod.rs
@@ -16,12 +16,13 @@ pub async fn run() {
     loop {
         clear_background(WHITE);
 
-        let rotation = 1.0 * macroquad::time::get_time() as f32;
-        draw_circle_with_line(200.0, 256.0, 100.0, rotation * 2.0);
+        let rotation = 0.05 * macroquad::time::get_time() as f32;
+        let gear_ratio = FRONT_GEAR_SIZE / REAR_GEAR_SIZE;
+        draw_wheel(200.0, 256.0, 100.0, rotation * FRONT_GEAR_SIZE);
 
-        draw_circle_with_line(200.0, 256.0, 15.0, rotation * 2.0);
+        draw_circle_with_line(200.0, 256.0, REAR_GEAR_SIZE, rotation * FRONT_GEAR_SIZE);
 
-        draw_circle_with_line(400.0, 256.0, 40.0, rotation * 0.75);
+        draw_circle_with_line(400.0, 256.0, FRONT_GEAR_SIZE, rotation * REAR_GEAR_SIZE);
 
         draw_text(
             &format!("fps: {}", macroquad::time::get_fps()),
@@ -32,6 +33,19 @@ pub async fn run() {
         );
 
         next_frame().await
+    }
+}
+
+fn draw_wheel(x: f32, y: f32, radius: f32, rotation: f32) {
+    draw_poly_lines(x, y, 30, radius, rotation, 1.0, GRAY);
+
+    let spoke_count = 24;
+
+    for i in 0..spoke_count {
+        let t = i as f32 * 2.0 * std::f32::consts::PI / spoke_count as f32;
+        let x2 = x + radius * (rotation + t).cos();
+        let y2 = y + radius * (rotation + t).sin();
+        draw_line(x, y, x2, y2, 1.0, GRAY);
     }
 }
 
