e7ecd98f3348c2389374884f09995065c3b0770b
zehreken <zehreken@gmail.com>
2023-01-19 23:16:06 +0100

    Add audio prototype for wasm

---
+++ src/audio/mod.rs
@@ -0,0 +1,57 @@
+use std::io::Read;
+
+use macroquad::{audio::*, prelude::*};
+const SIZE: usize = 1024 * 2;
+
+pub fn get_config() -> Conf {
+    Conf {
+        window_title: "Audio".to_owned(),
+        window_width: 800,
+        window_height: 600,
+        fullscreen: false,
+        ..Default::default()
+    }
+}
+
+pub async fn run() {
+    let raw: Vec<[f32; 2]> = vec![[0.0; 2]; SIZE];
+    let data: Vec<u8> = raw
+        .iter()
+        .flat_map(|e| e.iter().flat_map(|f| f.to_ne_bytes()))
+        .collect();
+    let data = &data[..];
+    // let sound = load_sound_from_bytes(data).await;
+    let sound = load_sound("test.wav").await;
+    let sound = sound.unwrap();
+
+    loop {
+        clear_background(WHITE);
+
+        egui_macroquad::ui(|egui_ctx| {
+            egui::SidePanel::left("").show(egui_ctx, |ui| {
+                if ui.button("test").clicked() {
+                    println!("test");
+                    play_sound(
+                        sound,
+                        PlaySoundParams {
+                            looped: false,
+                            volume: 1.0,
+                        },
+                    );
+                }
+            });
+        });
+
+        egui_macroquad::draw();
+
+        draw_text(
+            &format!("fps: {}", macroquad::time::get_fps()),
+            2.0,
+            12.0,
+            16.0,
+            PINK,
+        );
+
+        next_frame().await
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // cargo build --release --target wasm32-unknown-unknown
 
+mod audio;
 mod cycle_drive_train;
 mod graphics_functions;
 mod life;
@@ -9,12 +10,14 @@ use macroquad::prelude::Conf;
 async fn main() {
     // let future = life::run();
     // let future = cycle_drive_train::run();
-    let future = graphics_functions::run();
+    // let future = graphics_functions::run();
+    let future = audio::run();
     future.await
 }
 
 fn config() -> Conf {
     // life::get_config()
     // cycle_drive_train::get_config()
-    graphics_functions::get_config()
+    // graphics_functions::get_config()
+    audio::get_config()
 }
