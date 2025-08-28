5bf4890287a6db685476c8fb52630b9553a9a61d
zehreken <zehreken@gmail.com>
2025-08-28 11:14:07 +0200

    Add base for a_star module

---
+++ src/a_star/mod.rs
@@ -0,0 +1,21 @@
+use crate::app::App;
+
+pub fn get_title() -> String {
+    return "AStar".to_owned();
+}
+
+pub struct AStar {}
+
+impl AStar {
+    pub fn new(width: f32, height: f32) -> Self {
+        Self {}
+    }
+}
+
+impl App for AStar {
+    fn update(&mut self) {}
+
+    fn draw(&self) {}
+
+    fn resize(&mut self, width: f32, height: f32) {}
+}
