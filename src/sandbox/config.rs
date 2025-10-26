16404b32f1f432d7e28dbcac1f26b42cbe7f707c
zehreken <zehreken@gmail.com>
2025-10-26 23:00:03 +0100

    Improve sandbox example

--- src/sandbox/config.rs
+++ src/sandbox/config.rs
@@ -1,1 +1,3 @@
 pub const PARTICLE_SIZE: u32 = 4;
+pub const BRUSH_SIZES: &[u8] = &[1, 2, 4, 8, 16];
+pub const BRUSH_LABELS: &[&str] = &["1", "2", "4", "8", "16"];
--- src/sandbox/mod.rs
+++ src/sandbox/mod.rs
@@ -24,6 +24,8 @@ pub struct Sandbox {
     column_count: u32,
     particle_model: ParticleModel,
     image: Image,
+    brush_id: usize,
+    brush_size: u8,
     particle_id: u8,
 }
 
@@ -36,6 +38,8 @@ impl Sandbox {
             column_count: width as u32 / PARTICLE_SIZE,
             particle_model: ParticleModel::new(column_count, row_count),
             image: Image::gen_image_color(column_count as u16, row_count as u16, WHITE),
+            brush_id: 0,
+            brush_size: 0,
             particle_id: 0,
         }
     }
@@ -46,7 +50,7 @@ impl App for Sandbox {
         widgets::Window::new(
             hash!(),
             vec2(screen_width() / 2.0, screen_height() / 2.0),
-            vec2(160.0, 40.0),
+            vec2(158.0, 64.0),
         )
         .label("Controls")
         .titlebar(true)
@@ -66,9 +70,27 @@ impl App for Sandbox {
             if ui.button(None, "Clear") {
                 self.particle_model.clear();
             }
+            ui.combo_box(hash!(), "Brush size", BRUSH_LABELS, &mut self.brush_id);
+            self.brush_size = BRUSH_SIZES[self.brush_id];
         });
 
         self.particle_model.update();
+
+        for row in 0..self.row_count {
+            for column in 0..self.column_count {
+                let index = row * self.column_count + column;
+                if let Some(p) = &self.particle_model._particles[index as usize] {
+                    self.image
+                        .set_pixel(column, row, Color::from_hex(p.get_properties().color));
+                } else {
+                    self.image.set_pixel(column, row, WHITE);
+                }
+            }
+        }
+
+        if root_ui().is_mouse_over(mouse_position().into()) {
+            return;
+        }
 
         if is_key_pressed(KeyCode::Key1) {
             self.particle_id = 0;
@@ -85,23 +107,23 @@ impl App for Sandbox {
             let row = y as u32 / PARTICLE_SIZE;
             let column = x as u32 / PARTICLE_SIZE;
 
-            let index = row * self.column_count + column;
-            self.particle_model._particles[index as usize] = match self.particle_id {
-                0 => Some(Box::new(Sand::new())),
-                1 => Some(Box::new(Water::new())),
-                2 => Some(Box::new(Rock::new())),
-                _ => None,
-            };
-        }
+            let half_brush_size = (self.brush_size / 2) as u32;
+            let row_start = row - half_brush_size;
+            let row_range = row_start..row_start + self.brush_size as u32;
+            let column_start = column - half_brush_size;
+            let column_range = column_start..column_start + self.brush_size as u32;
 
-        for row in 0..self.row_count {
-            for column in 0..self.column_count {
-                let index = row * self.column_count + column;
-                if let Some(p) = &self.particle_model._particles[index as usize] {
-                    self.image
-                        .set_pixel(column, row, Color::from_hex(p.get_properties().color));
-                } else {
-                    self.image.set_pixel(column, row, WHITE);
+            for r in row_range {
+                for c in column_range.clone() {
+                    if r >= 0 && r < self.row_count && c >= 0 && c < self.column_count {
+                        let index = r * self.column_count + c;
+                        self.particle_model._particles[index as usize] = match self.particle_id {
+                            0 => Some(Box::new(Sand::new())),
+                            1 => Some(Box::new(Water::new())),
+                            2 => Some(Box::new(Rock::new())),
+                            _ => None,
+                        };
+                    }
                 }
             }
         }
